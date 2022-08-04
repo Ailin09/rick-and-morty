ed271d6c01af911915a16e26d59be793b59c8e15
German Valverde <german.valverde@externos.w2m.com>
2022-08-04 07:00:46 -0300

    replace browserRouter with hashRouter for github pages

--- rick-and-morty/src/App.js
+++ rick-and-morty/src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Characters from "./pages/Characters";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import CharacterDetail from "./pages/CharacterDetail";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Characters />} />
         <Route path="/character/:id" element={<CharacterDetail />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 export default App;
